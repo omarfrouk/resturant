2a7fdcb067fe8e003b4bbf1237536a77114971e0
omar_alfroukh <oalfroukh@asaltech.com>
2021-03-08 15:20:59 +0200

    add header and footer

--- resturant/src/components/Header/header.js
+++ resturant/src/components/Header/header.js
@@ -1,18 +1,15 @@
 import React from 'react'
+import './header.css'
 
 function header(props) {
     return (
-        <div>
-            <div className="header"> 
-                {/* <a href="">TREMONT  </a>  
-                <a href=""><span>Order Now </span></a> 
-                <a href="">Home </a> 
-                <a href="">Gallery </a>
-                <a href="">Story </a> 
-                <a href="">Visit </a>  
- */}
-
-            </div>
+        <div  className="header"> 
+                <a href="/home" className="navHunter">HUNTER</a>
+                <a href="" className="navMenu">Menu</a> 
+                <a href="" className="nav">Reservations</a>  
+                <a href="" className="nav">Order Now</a> 
+                <a href="" className="nav">Story</a> 
+                <a href="" className="nav">Gallery </a> 
         </div>
     )
 }
--- resturant/src/containers/Home/home.js
+++ resturant/src/containers/Home/home.js
@@ -1,7 +1,7 @@
-import React, { PureComponent } from 'react'
+import React  from 'react'
 import Header from "../../components/Header/header"
 import Footer from "../../components/Footer/footer"
-import HomeBack from "../../components/Footer/homeBack"
+import HomeBack from "../../components/HomeBack/homeBack"
 import './home.css'
 class home extends React.Component {
     constructor(props) {
